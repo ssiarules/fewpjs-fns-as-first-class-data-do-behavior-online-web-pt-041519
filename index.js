d240073b738569117a96277c7a5966299aae7fcf
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-05 01:26:52 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+//greet function receives string of time
+function greet(time){
+  // remove: from time string and parse single integer, set this to timeNow variable
+  let timeNow = parseInt(time.split(':'))
+  // if timeNow is less than 12 return and stop
+  if (timeNow < 12 ){
+    return "Good Morning"
+    //if timeNow is greater than 5 return and stop
+  } else if (timeNow - 12 > 5) {
+    return "Good Evening"
+    // if timeNow has not stopped, return and stop
+  }else{
+    return "Good Afternoon"
+  }
+}
+
+// displayMessage function receives string as argument
+function displayMessage(text){
+  // find text containing id of greeting
+  let content = document.getElementById('greeting')
+  // update text associated with greeting id
+  content.innerText = text;
+}
